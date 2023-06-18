16b8fc9ff7d15ac79700b518eb470c9cc4e8078b
Dean <73099456+Syntax-7@users.noreply.github.com>
2023-06-18 19:49:36 +0300

    Delete PycharmProjects directory

--- PycharmProjects/WorldOfGames/Utils.py
+++
@@ -1,8 +0,0 @@
-SCORES_FILE_NAME = "Scores.txt"
-BAD_RETURN_CODE = -1
-import os
-
-
-def Screen_cleaner():
-    # Clear the screen based on the operating system
-    os.system('cls' if os.name == 'nt' else 'clear')
